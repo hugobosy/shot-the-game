81a0e0c157b16d337620fdfb6f9a27ed322a1bd1
HugoBosy <hubert.kliszcz@gmail.com>
2023-01-17 08:59:16 +0100

    add swiper slider to hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "./modules/header/Header";
+import { Hero } from "./modules/hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
--- src/modules/hero/Hero.tsx
+++ src/modules/hero/Hero.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Wrapper } from "./Hero.styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export const Hero = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Swiper slidesPerView={1} spaceBetween={50}>
+        <SwiperSlide>Slide1</SwiperSlide>
+      </Swiper>
+    </Wrapper>
+  );
 };
