1305f05642a3de8377b4e04d9b22f020bc613a34
HugoBosy <hubert.kliszcz@gmail.com>
2023-01-17 09:27:51 +0100

    delete hero styles and types

--- src/modules/hero/Hero.tsx
+++ src/modules/hero/Hero.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import { Wrapper } from "./Hero.styles";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import { Container } from "../../ui/components/container/Container";
 
 export const Hero = () => {
-  return (
-    <Wrapper>
-      <Container>
-        <Swiper slidesPerView={1} spaceBetween={50}>
-          <SwiperSlide>Slide1</SwiperSlide>
-        </Swiper>
-      </Container>
-    </Wrapper>
-  );
+  return <div></div>;
 };
