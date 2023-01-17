32ed78cad4a16abd5d9d2d126226b6b055f16a0a
HugoBosy <hubert.kliszcz@gmail.com>
2023-01-17 09:49:14 +0100

    add react-slick

--- src/modules/hero/Hero.tsx
+++ src/modules/hero/Hero.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Hero = () => {
-  return <div></div>;
+  const setting = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div>
+      <h2>Slider</h2>
+      <Slider {...setting}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>1</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 };
